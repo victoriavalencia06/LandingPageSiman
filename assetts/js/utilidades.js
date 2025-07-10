c17878a10d9cab8c5369e8c4e432b368623febfc
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-07-09 23:54:42 -0600

    Correcciones en vistas

--- assetts/js/utilidades.js
+++ assetts/js/utilidades.js
@@ -1,12 +1,13 @@
+document.querySelectorAll('.rebajas-wrapper').forEach(wrapper => {
+    const contenedor = wrapper.querySelector('.rebajas-contenedor');
+    const left = wrapper.querySelector('.slider-btn.left');
+    const right = wrapper.querySelector('.slider-btn.right');
 
-const contenedor = document.querySelector('.rebajas-contenedor');
-const left = document.querySelector('.slider-btn.left');
-const right = document.querySelector('.slider-btn.right');
+    left.addEventListener('click', () => {
+        contenedor.scrollBy({ left: -contenedor.offsetWidth, behavior: 'smooth' });
+    });
 
-left.addEventListener('click', () => {
-    contenedor.scrollBy({ left: -contenedor.offsetWidth, behavior: 'smooth' });
-});
-
-right.addEventListener('click', () => {
-    contenedor.scrollBy({ left: contenedor.offsetWidth, behavior: 'smooth' });
+    right.addEventListener('click', () => {
+        contenedor.scrollBy({ left: contenedor.offsetWidth, behavior: 'smooth' });
+    });
 });
